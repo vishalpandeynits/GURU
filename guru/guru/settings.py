249983c000c6f6a0d99aea340409a6c2ff0be4ac
vishal <vishalpandeyviptsk@gmail.com>
2020-08-30 01:03:51 +0530

    settings changed

--- guru/guru/settings.py
+++ guru/guru/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_email_verification',
     'basic.apps.BasicConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
