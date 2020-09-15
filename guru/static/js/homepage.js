11e75283b4d7f3c944a9629cc75dc4e1c48e536d
vishal <vishalpandeyviptsk@gmail.com>
2020-09-15 14:25:30 +0530

    workin on this class page

--- guru/static/js/homepage.js
+++ guru/static/js/homepage.js
@@ -3,7 +3,7 @@ if ( window.history.replaceState ) {
 }
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "275px";
 }
 
 function closeNav() {
