e37859a2cfb0b750b324948f677257a217052ab6
vishalpandeyvip <vishalpandeyviptsk@gmail.com>
2020-12-24 15:55:30 +0530

    regex issue completed

--- static/js/validate.js
+++ static/js/validate.js
@@ -3,7 +3,6 @@ REGX= {
     'nameREGX':/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
     'emailREGX':/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
     'phone_number':/^\+(91)[6-9]\d{9}$/g,
-    "strong_password":new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"),
     'description_check':/^.{100,}/,
     'username':/^.{8,16}/
 }
@@ -16,45 +15,47 @@ function signUpPageValidation(){
     var first_name = document.querySelector('input[name="first_name"]').value;
     var last_name = document.querySelector('input[name="last_name"]').value;
     var email = document.querySelector('input[name="email"]').value;
-
     var usernameCheck = REGX['username'].test(username);
     if(!usernameCheck){
-        errors.push('Username must be between 8 to 16 characters long');
+        errors.push('Username must be between 8 to 16 characters long.');
     }
-
     var firstNameCheck = REGX['nameREGX'].test(first_name);
     if(!firstNameCheck){
-        errors.push('Please Enter a valid first name without any special character or digit');
+        errors.push('First name should not contain any special character or digit.');
     }
-
     var lastNameCheck = REGX['nameREGX'].test(last_name);
     if(!lastNameCheck){
-        errors.push('Please Enter a valid last name without any special character or digit');
+        errors.push('Last name should not contain any special character or digit.');
     }
-
     var emailCheck =  REGX['emailREGX'].test(email);
     if(!emailCheck){
-        errors.push('PLease enter a valid email');
+        errors.push('Please enter a valid email.');
     }
-
     if (password != confirm_password){
-        errors.push('Password and Confirm Password are different');
+        errors.push('Password and Confirm Password are different.');
     }
-
-    return false;
+    txt = ""
+    errors.forEach(signUpErrors);
+    if(txt === "") return true;
+    else{
+        _('error-log').innerHTML = txt;
+        return false;
+    }
+}
+function signUpErrors(value, index, array) {
+    txt = txt + '<li style="color:red;font-weight:bold;font-size:0.85em;">*' + value + '</li>'
 }
 
 function logInPageValidation(){
-
+    var username = document.querySelector('input[name="username"]').value;
+    var usernameCheck = REGX['username'].test(username);
+    if(!usernameCheck){
+        _('login-error').innerHTML = '<p style="color:red;">Username must be between 8 to 16 characters long</p>';
+        return false;
+    }
+    return true;
 }
 
-function ForgotPasswordValidation(){
-
-}
-
-function PasswordResetValidation(){
-
-}
 function TogglePassword(){  
     var x = document.querySelector('input[name="password1"]');
     var y = document.querySelector('input[name="password2"]');
@@ -89,3 +90,8 @@ function changePassword(){
     }
 }
 
+function validateAddSubject(){
+    var teacher = document.getElementsByName('teacher')[0].value;
+    if(teacher=='Please Choose')  return false;
+    else return true;
+}
