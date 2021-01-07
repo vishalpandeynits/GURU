d2effca93e991209eb6f4761baf6d1eb4459a2f2
vishalpandeyvip <vishalpandeyviptsk@gmail.com>
2021-01-07 12:08:38 +0530

    minor ui fixes

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from basic.models import Classroom
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	bio = models.TextField(default='',max_length=100, null=True, blank=True)
+	bio = models.CharField(default='',max_length=100, null=True, blank=True)
 	profile_pic = models.ImageField(default='avatar.jpg', upload_to='profile_pics')
 	phone_number = models.CharField(max_length=13,null=True,blank=True)
 	whatsapp_number = models.CharField(max_length=13,null=True,blank=True)
